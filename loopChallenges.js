728aaf20f62f324b4e404bdcf1b9c7d0c0f72900
Jared Green <jag9292@yahoo.com>
2021-04-25 08:47:15 -0400

    first

---
+++ loopChallenges.js
@@ -0,0 +1,22 @@
+for (var i=0; i<=20; i++)
+    if(i % 2 == 1) {
+    console.log(i);
+}
+
+for (var i=4; i>-4; i-=1.5) {
+    console.log(i);
+}
+
+var sum=0
+for (var i=1; i<=100; i++)
+    {sum += i;}
+    {
+    console.log(sum);
+}
+
+
+var n=1
+for (var i=1; i<1e8; i++)
+    {n *= i;}
+
+console.log(n);
